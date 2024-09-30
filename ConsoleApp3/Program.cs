91214906c2bf41e9f567dc925f932b3556ba360d
iremr <iremr@LAKICI53>
2024-09-30 13:55:36 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+
+    class İREMRAKICI
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İREM RAKICI");
+            Console.WriteLine("Patikanın uzman kadrosu ile hayallerime kavuşup emeklerimin karşılığını alabileceğimi düşünüyorum");
+            
+        }
+    }
+}
